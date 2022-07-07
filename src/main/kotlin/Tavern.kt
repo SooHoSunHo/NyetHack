2ecdd5baa8f021f11670dfd6aa780dc69d75fbc1
SooHoSunHo <lssmyhero@gmail.com>
2022-07-07 17:36:41 +0900

    non-null 단언 연산자

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,15 +1,21 @@
 fun main(args: Array<String>) {
 
 //    var beverage = readLine()?.replaceFirstChar { it.uppercase() }
-    var beverage = readLine()?.let {
-        if (it.isNotBlank()) {
-            it.replaceFirstChar { it.uppercase() }
-        } else {
-            "Beer"
-        }
-    }
+//    var beverage = readLine()?.let {
+//        if (it.isNotBlank()) {
+//            it.replaceFirstChar { it.uppercase() }
+//        } else {
+//            "Beer"
+//        }
+//    }
 //    beverage = null
 
-    println(beverage)
+    //var beverage = readLine()!!.replaceFirstChar { it.uppercase() }
+
+
+
+    val beverageService: String = beverage ?: "맥주"
+
+    println(beverageService)
 }
 
