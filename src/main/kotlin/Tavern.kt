b2d5f59c2bb791c48c778b51bcfcc64cee956ed7
SooHoSunHo <lssmyhero@gmail.com>
2022-07-08 11:16:33 +0900

    chapter7 문자열

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -1,3 +1,4 @@
+const val TAVERN_NAME = "Taernyl's Folly"
 fun main(args: Array<String>) {
 
 //    var beverage = readLine()?.replaceFirstChar { it.uppercase() }
@@ -10,13 +11,27 @@ fun main(args: Array<String>) {
 //    }
 //    beverage = null
 
-    var beverage = readLine()!!.replaceFirstChar { it.uppercase() }
+//    var beverage = readLine()!!.replaceFirstChar { it.uppercase() }
+//
+//
+//
+//    val beverageService: String = beverage ?: "맥주"
+//
+//    println(beverageService)
 
-
-
-    val beverageService: String = beverage ?: "맥주"
-
-    println(beverageService)
+    placeOrder("shandy, Dragon's Breath, 5.91")
 
 }
 
+private fun placeOrder(menuData: String) {
+    val indexOfApostrophe = TAVERN_NAME.indexOf('\'')
+    val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
+    println("마드리갈은 $tavernMaster 에게 주문한다.")
+
+    val data = menuData.split(",")
+    val type = data[0]
+    val name = data[1]
+    val price = data[2]
+    val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
+     println(message)
+}
