194f83379e678823ec48173de49cc22d3c09c105
SooHoSunHo <lssmyhero@gmail.com>
2022-07-08 13:18:47 +0900

    문자열 비교

--- src/main/kotlin/Tavern.kt
+++ src/main/kotlin/Tavern.kt
@@ -19,7 +19,7 @@ fun main(args: Array<String>) {
 //
 //    println(beverageService)
 
-    placeOrder("shandy, Dragon's Breath, 5.91")
+    placeOrder("shandy,Dragon's Breath,5.91")
 
 }
 
@@ -28,10 +28,29 @@ private fun placeOrder(menuData: String) {
     val tavernMaster = TAVERN_NAME.substring(0 until indexOfApostrophe)
     println("마드리갈은 $tavernMaster 에게 주문한다.")
 
-    val data = menuData.split(",")
-    val type = data[0]
-    val name = data[1]
-    val price = data[2]
+    /* 해체선언 */
+    val(type, name, price) = menuData.split(",")
     val message = "마드리갈은 금화 $price 로 $name ($type)를 구입한다."
-     println(message)
+    println(message)
+
+//    val phrase = "와, $name 진짜 좋구나!"
+//    println("마드리갈이 감탄한다: ${toDragonSpeak(phrase)}")
+    var phrase = if (name == "Dragon's Breath") {
+        "마드리갈이 감탄핟다: ${toDragonSpeak("와, $name 진짜 좋구나")}"
+    } else {
+        "마드리갈이 말한다: 감사합니다. $name"
+    }
+    println(phrase)
 }
+
+private fun toDragonSpeak(phrase: String) =
+    phrase.replace(Regex("[aeiou]")) {
+        when (it.value) {
+            "a" -> "4"
+            "e" -> "3"
+            "i" -> "1"
+            "o" -> "0"
+            "u" -> "|_|"
+            else -> it.value
+        }
+    }
